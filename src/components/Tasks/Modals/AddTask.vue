27398971d896568612d72acaf03f52520bb11119
ryowuandjanet <ryowudata@gmail.com>
2020-07-01 17:27:10 +0800

    chapter 8 half

---
+++ src/components/Tasks/Modals/AddTask.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-card>
+    <q-card-section class="row">
+      <div class="text-h6">Add Task</div>
+      <q-space />
+      <q-btn
+        v-close-popup
+        flat
+        round
+        dense
+        icon="close" />
+    </q-card-section>
+
+    <q-card-section class="q-pt-none">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis perferendis totam, ea at omnis vel numquam exercitationem aut, natus minima, porro labore.
+    </q-card-section>
+
+    <q-card-actions align="right">
+      <q-btn
+      flat
+      label="Save"
+      color="primary"
+      v-close-popup />
+    </q-card-actions>
+  </q-card> 
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
