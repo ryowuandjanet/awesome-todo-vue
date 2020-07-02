07ddc9c63d281cd41b6bf2ae423092b3f0f2ca1c
ryowuandjanet <ryowudata@gmail.com>
2020-07-02 18:08:18 +0800

    chapter 10 end(without refence data)

--- src/pages/PageTodo.vue
+++ src/pages/PageTodo.vue
@@ -1,16 +1,17 @@
 <template>
   <q-page class="q-pa-md">
-    <q-list
-      v-if ="Object.keys(tasks).length"
-      separator
-      bordered>
-      
-      <task
-        v-for="(task, key) in tasks"
-        :key="key"
-        :task="task"
-        :id="key" ></task>
-    </q-list>
+    
+    <no-tasks
+      v-if ="!Object.keys(tasksTodo).length"
+      @showAddTask="showAddTask = true"></no-tasks>
+
+    <tasks-todo
+      v-else
+      :tasksTodo="tasksTodo" />
+
+    <tasks-completed
+      v-if ="Object.keys(tasksCompleted).length"
+      :tasksCompleted="tasksCompleted" />
 
     <div class="absolute-bottom text-center q-mb-lg">
       <q-btn
@@ -38,11 +39,13 @@
       }
     },
     computed: {
-      ...mapGetters('tasks',['tasks'])
+      ...mapGetters('tasks',['tasksTodo','tasksCompleted'])
     },
     components: {
-      'task' : require('components/Tasks/Task.vue').default,
-      'add-task' : require('components/Tasks/Modals/AddTask.vue').default
+      'add-task' : require('components/Tasks/Modals/AddTask.vue').default,
+      'tasks-todo' : require('components/Tasks/TasksTodo.vue').default,
+      'tasks-completed' : require('components/Tasks/TasksCompleted.vue').default,
+      'no-tasks' : require('components/Tasks/NoTasks.vue').default,
     }
   }
 </script>
--- src/store/store-tasks.js
+++ src/store/store-tasks.js
@@ -53,8 +53,25 @@ const actions = {
 }
 
 const getters = {
-  tasks: (state) => {
-    return state.tasks
+  tasksTodo: (state) => {
+    let tasks = {}
+    Object.keys(state.tasks).forEach(function(key) {
+      let task =state.tasks[key]
+      if (!task.completed) {
+        tasks[key] = task
+      }
+    })
+    return tasks
+  },
+  tasksCompleted: (state) => {
+    let tasks = {}
+    Object.keys(state.tasks).forEach(function(key) {
+      let task =state.tasks[key]
+      if (task.completed) {
+        tasks[key] = task
+      }
+    })
+    return tasks
   }
 }
 
